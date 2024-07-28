28c27abb1ed795fab0c5f9c4c820100f389580f1
Christodoulos Ioannou <Christodoulos.Ioannou@gmail.com>
2024-07-29 00:00:19 +0300

    Changed the account_balance_low predicate and the description of the NL conclusions

--- nestor-ui.js
+++ nestor-ui.js
@@ -3,7 +3,7 @@ let contestDialogue = [
     ["Why is my loan application rejected?", "S02 :: suppose implies -reject_loan_application | 00;", "Loan Applicant", "N02"],
     ["Your loan application has been rejected because your care-giving obligations are considered high and your credit score is low.", "S03 :: suppose implies caregiving_obligations(high) | 00;\nS05 :: suppose implies credit_score(low) | 00;\nA01 :: caregiving_obligations(high), credit_score(low) implies reject_loan_application | 11;", "Bank Officer", "N03"],
     ["My loan application should not have been rejected because I am a good existing customer: I own an account for a long time and I make frequent transactions.", "A03 :: good_existing_customer implies -reject_loan_application | 13;\nP01 :: perceive implies account_owner_for_long | 21;\nP02 :: perceive implies transaction_frequency(high) | 22;\nA02 :: account_owner_for_long, transaction_frequency(high) implies good_existing_customer | 12;", "Loan Applicant", "N04"],
-    ["You are not qualified as a good existing customer because your account balance is low for more than one year.", "P03 :: perceive implies account_balance_low_more_than(1, year) | 23;\nA04 :: account_balance_low_more_than(1, year) implies -good_existing_customer | 24;", "Bank Officer", "N05"],
+    ["You are not qualified as a good existing customer because your account balance is low for more than one year.", "P03 :: perceive implies account_balance_low_for(1, year) | 23;\nA04 :: account_balance_low_for(1, year) implies -good_existing_customer | 24;", "Bank Officer", "N05"],
     ["Why is my credit score low?", "S06 :: suppose implies -credit_score(low) | 00;", "Loan Applicant", "N06"],
     ["Your credit score is considered low because it is 582.", "C01 :: credit_score_value(582) # credit_score_value(590);\nP07 :: perceive implies credit_score_value(582) | 27;\nA05 :: credit_score_value(582) implies credit_score(low) | 15;", "Bank Officer", "N07"],
     ["My credit score is 590.", "P08 :: perceive implies credit_score_value(590) | 28;", "Loan Applicant", "N08"],
@@ -23,7 +23,7 @@ let explanations = [
     ["Applicant owns an account for a long time. ", "P01", "account_owner_for_long", "Loan Applicant"],
     ["Applicant makes frequent transactions. ", "P02 ", "transaction_frequency(high)", "Loan Applicant"],
     ["Applicant is a good existing customer because applicant owns an account for a long time and makes frequent transactions. ", "A02", "good_existing_customer", "Loan Applicant"],
-    ["Applicant's account balance is low for more than one year. ", "P03", "account_balance_low_more_than(1, year)", "Bank Officer"],
+    ["Applicant's account balance is low for more than one year. ", "P03", "account_balance_low_for(1, year)", "Bank Officer"],
     ["Applicant is not a good existing customer because applicant's account balance is low for more than one year. ", "A04", "-good_existing_customer", "Bank Officer"],
     ["Applicant's credit score should not be considered low. ", "S06", "-credit_score(low)", "Loan Applicant"],
     ["Applicant's credit score is 582. ", "P07", "credit_score_value(582)", "Bank Officer"],
@@ -274,12 +274,12 @@ async function inferConclusionMain() {
 
 		evalResult = evaluateLoanApplication(responseJSON.inferences);
 		if (evalResult == 0)
-			suggestion = "Loan application rejection must be supported otherwise the decision will be dismissed!"
+			suggestion = "Loan application rejection must be further supported otherwise it will be dismissed!"
 		else {
 			if (evalResult == 1)
-				suggestion = "Loan application rejection is not justified!"
+				suggestion = "Loan application rejection is dismissed!"
 			else
-				suggestion = "Loan application rejection is justified!"
+				suggestion = "Loan application rejection is upheld!"
 
 			explanations.forEach(row => {
 				if (row.length >= 2 && conclusionExpressions.includes(row[1])) {
@@ -288,7 +288,7 @@ async function inferConclusionMain() {
 			});
 		}
 
-		let nlConclusions = "Suggestion: " + suggestion;
+		let nlConclusions = "Determination: " + suggestion;
 		nlConclusions += (nlJustification !== "") ? ("\n\nJustification:\n" + nlJustification) : "";
 
 		//document.getElementById("inferredNLConclusions").innerHTML = nlConclusions;
